b7a7298a8dd438e5e5fb4bd3e988a40051f97357
Sofia <sofiarodrigues45@gmail.com>
2022-09-19 19:07:05 -0300

    adicionando autenticação

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -2,23 +2,32 @@ import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import { IconContext } from 'react-icons/lib';
 
+import { signIn, signOut, useSession } from 'next-auth/react'
+
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const { data: session } = useSession()
 
-    return isUserLoggedIn ? (
-        <button type="button" className={styles.signInButton}>
+    return session ? (
+        <button 
+            type="button"
+            className={styles.signInButton}
+            onClick={() => signOut()}
+        >
             <FaGithub color='#04d361' />
-            Sofia Rodrigues
-
+            {session.user.name}
             <IconContext.Provider value={{ className: styles.closeIcon }}>
                 <FiX color='#737380' />
             </IconContext.Provider>
 
         </button>
     ) : (
-        <button type="button" className={styles.signInButton}>
+        <button
+            type="button"
+            className={styles.signInButton}
+            onClick={() => signIn('github')}
+        >
             <FaGithub color='#eba517' />
             Sign in with Github
         </button>
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,66 @@
+import { query as q } from 'faunadb'
+
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna'
+
+export const authOptions = {
+    providers: [
+      GithubProvider({
+        clientId: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        authorization: { params: { scope: "read:user" } },
+
+      }),
+    ],
+    callbacks: {
+        async signIn({ user, account, profile }) {
+            const { email, name, following } = user;
+
+            try {
+                await fauna.query(
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'),
+                                    q.Casefold(user.email)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('users'),
+                            {
+                                data: {
+                                    email: email,
+                                    name: name,
+                                    following: following,
+                                },
+                            },
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(user.email)
+                            )
+                        )
+                    )
+
+                )
+                .then((ret) => console.log(ret))
+                .catch((err) => console.error(
+                    'Error: [%s] %s: %s',
+                    err.name,
+                    err.message,
+                    err.errors()[0].description,
+                ))
+                return true
+            } catch {
+                return false
+            }
+        },
+    }
+}
+
+export default NextAuth(authOptions)
